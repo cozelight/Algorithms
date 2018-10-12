107d09c41a7ee12599be32c33cd7f8854dc79d7d
coze <cozelight@gmail.com>
2018-10-13 07:58:29 +0800

    Exercise 1.1.1

---
+++ 1-Fundamentals/1-1-BasicProgModel/Ex_1_1_01.swift
@@ -0,0 +1,21 @@
+//
+//  Ex_1_1_01.swift
+//  Fundamental
+//
+//  Created by coze on 2018/10/13.
+//  Copyright © 2018 cozelight. All rights reserved.
+//
+
+import Foundation
+
+class Ex_1_1_01 {
+    class func unitTest() {
+        let a = (0 + 15) / 2
+        let b = 2.0e-6 * 100000000.1
+        let c = true && false || true && true
+        
+        print(a)
+        print(b)
+        print(c)
+    }
+}
